4e50978e4b4613be3667b0a02f905ea9632fa3a0
Numberer <ybs1164@naver.com>
2023-07-16 08:16:15 +0000

    type annotion

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,18 @@ main = do
 
 
 type Position = (Float, Float)
+type GridPosition = (Int, Int)
 
 data Team = White | Black | Grey
 
 data Dir = U | D | L | R
+data Range = Int
+type Area = (GridPosition, Range)
+
+data Target = 
+    TargetCard Card 
+  | TargetObject Object
+  | TargetArea Area
 
 -- card (todo)
 data Card = Card {
@@ -17,11 +25,10 @@ data Card = Card {
   , cardCost   :: Int
   , cardAction :: [String] }
 
--- first : target selected
--- second : 
 
 type Map = (Int, Int)
 
+data Object = ObjectStone Stone | ObjectWall Wall
 
 data Stone = Stone {
     stonePosition :: Position
@@ -45,5 +52,21 @@ data Game = Game {
     player1  :: Player
   , player2  :: Player
   , turn     :: Bool
+  , map      :: Map
+  , objects  :: [Object]
   , gameName :: String }
 
+{--
+isObjectInArea :: Area -> Object -> Bool
+isObjectInArea (areaPosition, r) (ObjectStone stone) = (ax+ar >= sx) && (ax-ar <= sx) && (ay+ar >= sy) && (ay-ar <= sy)
+  where 
+    (ax, ay) = fmap fromIntegral areaPosition
+    ar       = fromIntegral r
+    (sx, sy) = stonePosition stone
+isObjectInArea ((x, y), r) (ObjectWall wall) = (x+r >= wx) && (x-r <= wx) && (y+r >= wy) && (y-r <= wy)
+  where 
+    (wx, wy) = wallPosition wall 
+--}
+
+-- getObjectsInArea :: [Object] -> Area -> [Object]
+-- getInAreaObjects objects area = filter (isObjectInArea area) objects 
